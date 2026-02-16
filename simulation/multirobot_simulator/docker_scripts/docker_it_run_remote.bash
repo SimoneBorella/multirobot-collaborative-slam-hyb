42cd811b286e9589fae7dcc83d351e7105465360
Borella Simone <borellasimone00@gmail.com>
2026-02-16 13:54:45 +0100

    Update

--- simulation/multirobot_simulator/docker_scripts/docker_it_run_remote.bash
+++ simulation/multirobot_simulator/docker_scripts/docker_it_run_remote.bash
@@ -54,12 +54,12 @@ docker run -it \
     --privileged \
     --volume $DOCKERFILE_PATH/ros2_ws:/ros2_ws \
     -p 6981:6080 \
+    --env VNC_PASSWORD="" \
     --name $IMAGE \
     --hostname $IMAGE \
     $IMAGE:$TAG \
     /usr/local/share/desktop-init.sh bash
 
-    # --env VNC_PASSWORD="password" \
 
 
     # --volume /dev/bus/usb:/dev/bus/usb \
